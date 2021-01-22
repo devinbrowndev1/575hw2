f39f44176574c58e79a572d1786b812b76852e74
devinbrowndev1 <devinbrowndev@gmail.com>
2021-01-22 09:19:09 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,36 @@ import sys
 import re
 import nltk
 
+
+class state:
+    def __init__(self):
+        self.state_name = ''
+        self.possible_input = []
+        self.next_states = []
+        self.sys_out = []
+        
+    def __str__(self):
+        return "STATE:{}\n SYS_OUT:{}\nPoss. Input:{}\n Next States:{}".format(self.state_name,self.sys_out,self.possible_input,self.next_states)
+        
+    def build_state(self,state_list):
+        self.state_name = state_list.pop(0)
+        self.sys_out = state_list.pop(0)
+        while len(state_list) > 0:
+            in_trans = state_list.pop().split(':')
+            self.possible_input.append(in_trans[0])
+            self.next_states.append(in_trans[1])
+
+#testing state class
+# test = ["start",
+# "Welcome to the pizza ordering system.\nPizzaBot:To cancel at anytime type: cancel, To repeat order at anytime type: repeat\nPizzaBot:What specialty pizza would you like?",
+# "hawaiian:add_pizza",
+# "vegan:add_pizza"]
+
+# s = state()
+# s.build_state(test)
+# print(s)
+
+
 #order class
 class pizzaOrder:
     def __init__(self):
