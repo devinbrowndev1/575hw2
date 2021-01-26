ad6c5a8c25969f8d95372861de3daa0afc31e2d7
Jessica Sweeney <jsweeney97@uchicago.edu>
2021-01-26 13:21:16 -0500

    made NLU more robust, added more types of pizza

--- NLUDefaultFrame.py
+++ NLUDefaultFrame.py
@@ -10,38 +10,39 @@ class NLU:
 
         returned_info = []
 
-        flavors = ["vegan", "hawaiian"]
+        flavors = ["vegan", "hawaiian", "meat lovers", "4 cheese", "pepperoni", "veggie supreme"]
         sizes = ["small", "medium", "large"]
         crusts = ["thin", "regular", "gluten-free", "deep-dish"]
         names = ["Jessica", "Devin", "Alex", "Alexandra"]
         addresses = ["195 Stevens Way", "185 Stevens Way", "155 Stevens Way"]
 
         for flavor in flavors:
-            if flavor in in_string:
+            if flavor in in_string.lower():
                 returned_info.append((flavor, "add_pizza"))
         for size in sizes:
-            if size in in_string:
+            if size in in_string.lower():
                 returned_info.append((size, "change_size"))
         for crust in crusts:
-            if crust in in_string:
+            check_format = re.sub("-", "", crust)
+            if crust in in_string.lower() or check_format in in_string.lower():
                 returned_info.append((crust, "change_crust"))
         for name in names:
-            if name in in_string:
+            if name in in_string or name.lower() in in_string:
                 returned_info.append((name, "add_name"))
 
-        if "delivery" in in_string:
+        if "delivery" in in_string.lower():
             returned_info.append(("delivery", "delivery_type"))
-        elif "pickup" in in_string or "pick-up" in in_string:
+        elif "pickup" in in_string.lower() or "pick-up" in in_string.lower():
             returned_info.append(("pickup", "delivery_type"))
 
-        if "reorder" in in_string:
+        if "reorder" in in_string.lower():
             returned_info.append(("reorder", "reorder"))
 
         if any(map(str.isdigit, in_string)):
             in_value = re.sub('[^0-9]', '', in_string)
             if len(in_value) == 10:
                 returned_info.append((in_value, "add_phone"))
-            else:
+            elif len(in_value) > 1:
                 returned_info.append(("123 Main Street", "add_address"))
 
         if "yes" == in_string or "Yes" == in_string:
